a5058666df0101edf4c6eae143174d779ff2e77f
Mohammed7assan <mh0056592@gmail>
2024-10-24 14:14:06 +0300

    first commit

---
+++ reports/src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+//Report Viewer source
+// Report Viewer source
+import '@boldreports/javascript-reporting-controls/Scripts/bold.report-viewer.min';  
+import '@boldreports/javascript-reporting-controls/Content/material/bold.reports.all.min.css';  
+
+// Data-Visualization
+import '@boldreports/javascript-reporting-controls/Scripts/data-visualization/ej.bulletgraph.min';
+import '@boldreports/javascript-reporting-controls/Scripts/data-visualization/ej.chart.min'; 
+
+// Reports react base
+import '@boldreports/react-reporting-components/Scripts/bold.reports.react.min'; 
+declare let BoldReportViewerComponent: any;
+var viewerStyle = {
+  'height': '700px',
+  'width': '100%'
+};
+function App() {
+  return (
+    <div style={viewerStyle}>
+     <BoldReportViewerComponent
+     id="reportviewer-container"
+     reportServiceUrl = {'https://demos.boldreports.com/services/api/ReportViewer'}
+     reportPath = {'~/Resources/docs/sales-order-detail.rdl'} >
+     </BoldReportViewerComponent>
+    </div>
+  );
+}
+
+export default App;
